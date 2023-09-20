14817b9168cebd4183fa2ade3033b5471437ae54
Marlon Osorio <marlon.osorio@lasamericas.com.co>
2023-09-20 06:54:59 -0500

    primer commit

---
+++ juanpa.php
@@ -0,0 +1,14 @@
+<?php
+include_once("conex.php");
+include_once("comun.php");
+
+//$wemp_plma = consultarAliasPorAplicacion($conex, '01', 'CentralCamas');
+//echo $wemp_plma;
+
+$wemp_plma = '01';
+
+//$prueba1 = consultarAliasAplicacion($conex, $wemp_plma, 'CentralCamas');
+
+
+
+?>
